31143a814354ad00f3fd22451009fd77d5980c5c
junghyeon <j01h02@gmail.com>
2024-05-05 11:26:13 -0700

    front login

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -23,5 +23,14 @@ function login() {
         body: JSON.stringify(req)
     })
         .then((res) => res.json())
-        .then((res) => console.log(res));
+        .then((res) => {
+            if (res.success) {
+                location.href = "/" // 루트 경로로 이동
+            } else {
+                alert(res.msg);
+            }
+        })
+        .catch((err) => {
+            console.error(new Error("로그인 중 에러 발생"));
+        });
 };
