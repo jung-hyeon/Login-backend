e8fb2a5a82d4a9308a513399917e4a72142e8e5c
junghyeon <j01h02@gmail.com>
2024-05-06 08:52:03 -0700

    connect AWS and RDS & dotenv

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,65 +1,30 @@
 "use strict";
 
-const { log } = require("console");
-
-const fs = require("fs").promises;
+const { reject } = require("async");
+const db = require("../config/db");
 
 class UserStorage {
-    static #getUsersInfo(data, id) {
-        const users = JSON.parse(data);
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users); // => {id, password, name}
-        const userInfo = usersKeys.reduce((newUser, info) => {
-            newUser[info] = users[info][idx];
-            return newUser;
-        }, {});
-    
-        return userInfo;
-    }
-
-    static #getUsers(data, isAll, fields) {
-        const users = JSON.parse(data);
-        if (isAll) return users;
-        const newUsers = fields.reduce((newUsers, field) => {
-            if (users.hasOwnProperty(field)) {
-                newUsers[field] = users[field];
-            }
-            return newUsers;
-        }, {});
-        return newUsers;
-    }
-
-    static getUsers(isAll, ...fields) {
-        return fs
-        .readFile("./src/database/users.json")
-        .then((data) => {
-            return this.#getUsers(data, isAll, fields);
-        })
-        .catch(console.error);
-    }
-
     static getUsersInfo(id) {
-        return fs
-            .readFile("./src/database/users.json")
-            .then((data) => {
-                return this.#getUsersInfo(data, id);
-            })
-            .catch(console.error);
+        return new Promise((resolve, reject) => {
+            const query = "SELECT * FROM users WHERE id = ?;";
+            db.query(query, [id],(err, data) => {
+                if (err) reject(`${err}`);
+                resolve(data[0]); 
+            });
+        });
     }
-
 
     static async save(userInfo) {
-        const users = await this.getUsers(true); // true이면 모든 데이터 가져오겠다.
-        console.log(users);
-        // 데이터 추가
-        if (users.id.includes(userInfo.id)) {
-            throw "이미 존재하는 아이디입니다.";
-        }
-        users.id.push(userInfo.id);
-        users.name.push(userInfo.name);
-        users.password.push(userInfo.password);
-        fs.writeFile("./src/database/users.json", JSON.stringify(users));
-        return {success: true};
+        return new Promise((resolve, reject) => {
+            const query = "INSERT INTO users(id, name, password) VALUES(?, ?, ?);";
+            db.query
+                (query,
+                [userInfo.id, userInfo.name, userInfo.password],
+                (err) => {
+                if (err) reject(`${err}`);
+                resolve({success: true}); 
+            });
+        });
     }
 }
 
